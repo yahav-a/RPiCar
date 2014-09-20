58a757404b1075bf42c4a049a4cce116a361034e
Yahav Azran <yahavaz@post.bgu.ac.il>
2014-09-20 14:23:28 +0300

    skeleton added

---
+++ protocol.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+Client=\
+	{
+		'INIT_HEY' : 100,
+		'INIT_DONE': 110,
+		'MOVE'	   : 200,
+		'KTHXBYE'  : 300
+	}
+
+Server=\
+	{
+		'INIT_OK' : 105,
+		'MOVED'	  : 205,
+		'ERROR'	  : 204,
+		'CLOSE'	  : 305
+	}
