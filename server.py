6222c0c8e85fe18fc46f915c0161002877b77c64
= <javitolin@gmail.com>
2014-09-20 13:24:54 +0000

    server changes

--- server.py
+++ server.py
@@ -11,38 +11,41 @@ s.bind((TCP_IP,TCP_PORT))
 print("Waiting for connection")
 try:
 	s.listen(1)
-	
 	conn,addr = s.accept()
-	conn.send(Server.INIT_OK)
-	print("Connected to: " + str(addr[0]) + str(addr[1]))
+	print("Connected to: " + str(addr[0]) +":"+ str(addr[1]))
 	while 1:
 		data = conn.recv(BUFFER_SIZE)
 		if not data: break
+		data = data.decode()
 		print("Data received: "+data)
-		formattedData = data.split(" ")
-		action = formattedData[0]
-		speed = int(formattedData[1])
-		response = Server.MOVED + ": "
-		if action == Movements.Straight:
-			#goStraight(speed)
-			response += "Moved Straight"
-		elif action == Movements.Back:
-			#goBack(speed)
-			response += "Moved Back"
-		elif action == Movements.Right:
-			#turnRight(speed)
-			response += "Turned Right"
-		elif action == Movements.Left:
-			#turnLeft(speed)
-			response += "Turned Left"
-		elif action == Movements.Halt:
-			#stop()
-			response += "Stopped"
-		elif action == Client.KTHXBYE:
-			conn.send(Server.CLOSE)
-			exitAndClean()
-		conn.send(response)
-	conn.send(Server.CLOSE)
+		if data[:3] == Client.INIT_HEY:
+			conn.send(Server.INIT_OK.encode())
+		else:
+			data = data[3:]
+			formattedData = data.split(" ")
+			action = formattedData[0]
+			speed = int(formattedData[1])
+			response = Server.MOVED + ": "
+			if action == Movements.Straight:
+				#goStraight(speed)
+				response += "Moved Straight"
+			elif action == Movements.Back:
+				#goBack(speed)
+				response += "Moved Back"
+			elif action == Movements.Right:
+				#turnRight(speed)
+				response += "Turned Right"
+			elif action == Movements.Left:
+				#turnLeft(speed)
+				response += "Turned Left"
+			elif action == Movements.Halt:
+				#stop()
+				response += "Stopped"
+			elif action == Client.KTHXBYE:
+				conn.send(Server.CLOSE.encode())
+				exitAndClean()
+			conn.send(response.encode())
+	conn.send(Server.CLOSE.encode())
 	exitAndClean()
 except KeyboardInterrupt:
 	print("Rage Quit")
